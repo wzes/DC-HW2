cc5925f32192e22a5b128550df7cb46f9a1c5149
wzes <793890838@qq.com>
2017-12-03 02:36:05 +0800

    fix some bugs

--- src/main/java/com/wzes/dc/TestApplication.java
+++ src/main/java/com/wzes/dc/TestApplication.java
@@ -51,76 +51,76 @@ public class TestApplication {
 //            }
 //
 //        }
-//        File inFile = new File("/home/xuantang/Downloads/slide.db");
-//        File outFile = new File("/home/xuantang/Downloads/slide.zip");
-//        try {
-//            FileInputStream fileInputStream = new FileInputStream(inFile);
-//            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
-//
-//            int len = fileInputStream.available();
-//            byte[] conent = new byte[len];
-//            byte[] buf = new byte[1024];
-//            int count;
-//            int index = 0;
-//            while ((count = bufferedInputStream.read(buf)) != -1) {
-//                System.arraycopy(buf, 0, conent, index*1024, count);
-//                index++;
-//            }
-//            byte[] compress = GzipUtils.compress(conent);
-//
-//            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(outFile));
-//
-//            assert compress != null;
-//            bufferedOutputStream.write(compress);
-//
-//            bufferedOutputStream.close();
-//            bufferedInputStream.close();
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//      File file = new File("iframe.html");
-
-        String url_path = "http://www.sse.com.cn/assortment/stock/list/share/";
-        String root_path = null;
+        File inFile = new File("/d1/input/slide.db");
+        File outFile = new File("/d1/input/slide.zip");
         try {
-            URL url = new URL(url_path);
-            root_path = url.getProtocol() + "://" + url.getHost();
-            System.out.println(root_path);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+            FileInputStream fileInputStream = new FileInputStream(inFile);
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
 
-        try {
-            FileReader fileReader = new FileReader("iframe.html");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line;
-            StringBuilder stringBuilder = new StringBuilder();
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line).append('\n');
+            int len = fileInputStream.available();
+            byte[] conent = new byte[len];
+            byte[] buf = new byte[1024];
+            int count;
+            int index = 0;
+            while ((count = bufferedInputStream.read(buf)) != -1) {
+                System.arraycopy(buf, 0, conent, index*1024, count);
+                index++;
             }
-            String content = stringBuilder.toString();
-            int index = content.indexOf("href=\"/");
-            System.out.println(index);
-            String regEx1="href=\"/";
-            Pattern pattern1 = Pattern.compile(regEx1);
-            Matcher matcher1 = pattern1.matcher(content);
-            String r1 = matcher1.replaceAll("href=\"" + root_path + "/");
-            String regEx2="src=\"/";
-            Pattern pattern2 = Pattern.compile(regEx2);
-            Matcher matcher2 = pattern2.matcher(r1);
-            String r2 = matcher2.replaceAll("src=\"" + root_path + "/");
-            System.out.println(r2);
+            byte[] compress = GzipUtils.compress(conent);
 
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(outFile));
 
-            bufferedReader.close();
-            fileReader.close();
+            assert compress != null;
+            bufferedOutputStream.write(compress);
+
+            bufferedOutputStream.close();
+            bufferedInputStream.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+//      File file = new File("iframe.html");
+
+//        String url_path = "http://www.sse.com.cn/assortment/stock/list/share/";
+//        String root_path = null;
+//        try {
+//            URL url = new URL(url_path);
+//            root_path = url.getProtocol() + "://" + url.getHost();
+//            System.out.println(root_path);
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            FileReader fileReader = new FileReader("iframe.html");
+//            BufferedReader bufferedReader = new BufferedReader(fileReader);
+//            String line;
+//            StringBuilder stringBuilder = new StringBuilder();
+//            while ((line = bufferedReader.readLine()) != null) {
+//                stringBuilder.append(line).append('\n');
+//            }
+//            String content = stringBuilder.toString();
+//            int index = content.indexOf("href=\"/");
+//            System.out.println(index);
+//            String regEx1="href=\"/";
+//            Pattern pattern1 = Pattern.compile(regEx1);
+//            Matcher matcher1 = pattern1.matcher(content);
+//            String r1 = matcher1.replaceAll("href=\"" + root_path + "/");
+//            String regEx2="src=\"/";
+//            Pattern pattern2 = Pattern.compile(regEx2);
+//            Matcher matcher2 = pattern2.matcher(r1);
+//            String r2 = matcher2.replaceAll("src=\"" + root_path + "/");
+//            System.out.println(r2);
+//
+//
+//            bufferedReader.close();
+//            fileReader.close();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public static String replaceAll(String urlPath, String xml) {
